6626730b00d0a517be72e14778e48afc0aeb9d67
Joshua Harris <joshua.jharris13@gmail.com>
2018-06-25 14:23:00 -0400

    Merge.

--- src/directives/picker.js
+++ src/directives/picker.js
@@ -156,20 +156,6 @@ export default function picker() {
         return false;
       }
 
-      function canMove(from, numOfItems) {
-        if (
-          (
-            from === 'left' &&
-            numOfItems < vm.limit
-          ) ||
-          from === 'right' ||
-          !vm.limit
-        ) {
-          return true;
-        }
-        return false;
-      }
-
       vm.move = function(to, item) {
         if (!item) {
           return;
